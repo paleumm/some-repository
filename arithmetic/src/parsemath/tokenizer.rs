bb3b15edeaddf6023c1da333e33eff258a2c140e
paleumm <zdotqdot@gmail.com>
2022-10-30 00:55:36 +0700

    Update arithmetic

--- arithmetic/src/parsemath/tokenizer.rs
+++ arithmetic/src/parsemath/tokenizer.rs
@@ -1,6 +1,8 @@
 use std::iter::Peekable;
 use std::str::Chars;
 
+use super::token::Token;
+
 pub struct Tokenizer<'a> {
     expr: Peekable<Chars<'a>>,
 }
@@ -24,11 +26,25 @@ impl<'a> Iterator for Tokenizer<'a> {
 
                 while let Some(next_char) = self.expr.peek() {
                     if next_char.is_numeric() || next_char == &'.' {
-                        number.push(self.expr.next()?);
+                        num.push(self.expr.next()?);
                     } else if next_char == &'(' {
+                        return None;
+                    } else {
+                        break;
                     }
                 }
+
+                Some(Token::Num(num.parse::<f64>().unwrap()))
             }
+            Some('+') => Some(Token::Add),
+            Some('-') => Some(Token::Subtract),
+            Some('*') => Some(Token::Multiply),
+            Some('/') => Some(Token::Divide),
+            Some('^') => Some(Token::Caret),
+            Some('(') => Some(Token::LeftParen),
+            Some(')') => Some(Token::RightParen),
+            None => Some(Token::EOF),
+            Some(_) => None,
         }
     }
 }
