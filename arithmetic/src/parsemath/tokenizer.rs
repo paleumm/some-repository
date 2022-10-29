26977749d035b054438a1a4be888add9ec755763
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-10-29 17:03:04 +0700

    Update tokenizer.rs

--- arithmetic/src/parsemath/tokenizer.rs
+++ arithmetic/src/parsemath/tokenizer.rs
@@ -1,3 +1,34 @@
-pub struct Tokenizer {
-    expr: std::str::Chars,
+use std::iter::Peekable;
+use std::str::Chars;
+
+pub struct Tokenizer<'a> {
+    expr: Peekable<Chars<'a>>,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(new_expr: &'a str) -> Self {
+        Tokenizer {
+            expr: new_expr.chars().peekable(),
+        }
+    }
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Token> {
+        let next_char = self.expr.next();
+
+        match next_char {
+            Some('0'..='9') => {
+                let mut num = next_char?.to_string();
+
+                while let Some(next_char) = self.expr.peek() {
+                    if next_char.is_numeric() || next_char == &'.' {
+                        number.push(self.expr.next()?);
+                    } else if next_char == &'(' {
+                    }
+                }
+            }
+        }
+    }
 }
