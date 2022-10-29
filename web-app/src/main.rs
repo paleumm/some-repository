a9a9b988593cf337bb8ad459245452cc3a18029f
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-10-29 15:06:56 +0700

    Create main.rs

---
+++ web-app/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+struct AppState {
+    todolist_entries: Mutex<Vec<TodolsitEntry>>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TodolsitEntry {
+    id: i32,
+    date: i64,
+    title: String,
+}
+
+fn main() {}
