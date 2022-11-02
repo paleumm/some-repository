79ca69eb3f1344f0efa02da6f015c4cb1925be1d
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-11-02 21:48:38 +0700

    Create main.rs

---
+++ rstat/src/main.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+mod errors;
+use errors::StatsError;
+mod srcstats;
+use srcstats::get_summary_src_stats;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "rstat",
+    about = "This is a tool to generate statistics on Rust projects"
+)]
+struct Opt {
+    #[structopt(name = "source directory", parse(from_os_str))]
+    in_dir: PathBuf,
+    #[structopt(name = "mode", short)]
+    mode: String,
+}
+
+fn main() -> Result<(), StatsError> {
+    let opt = Opt::from_args();
+    let mode = &opt.mode[..];
+    match mode {
+        "src" => {
+            let stats = get_summary_src_stats(&opt.in_dir)?;
+            println!("Summary stats: {:?}", stats);
+        }
+        _ => println!("No stats..."),
+    }
+    Ok(())
+}
