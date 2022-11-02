71339c8dc34a27dbb834b93cf3a2eead4aed7f0f
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-11-02 11:09:21 +0700

    Update main.rs

--- template-engine-dyn/src/main.rs
+++ template-engine-dyn/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+use std::io::{self, BufRead};
+use template_engine_dyn::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut context: HashMap<String, String> = HashMap::new();
+
+    context.insert("name".to_string(), "Bob".to_string());
+    context.insert("city".to_string(), "Boston".to_string());
+
+    for line in io::stdin().lock().lines() {
+        match get_content_type(&line.unwrap().clone()) {
+            ContentType::TemplateVariable(mut content) => {
+                let html = generate_html_template_var(&mut content, context.clone());
+                println!("{}", html.gen_html);
+            }
+            ContentType::Literal(text) => println!("{}", text),
+            ContentType::Tag(TagType::ForTag) => println!("For Tag not implemented"),
+            ContentType::Tag(TagType::IfTag) => println!("If Tag not implemented"),
+            ContentType::Unrecognized => println!("Unrecognized input"),
+        }
+    }
 }
