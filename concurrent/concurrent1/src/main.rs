ab9d6137f2c2d1ed7f142257b37ea72d0b8c7757
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-11-08 21:19:58 +0700

    Update main.rs

--- concurrent/concurrent1/src/main.rs
+++ concurrent/concurrent1/src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut child_threads = Vec::new();
+
+    for _ in 1..5 {
+        let handles = thread::spawn(|| {
+            println!("Hi from thread id {:?}", thread::current().id());
+        });
+
+        child_threads.push(handles);
+    }
+    for i in child_threads {
+        i.join().unwrap();
+    }
 }
