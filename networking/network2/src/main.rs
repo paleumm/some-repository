eaf272b056817fcc1dd4348cb67b9f0260ea4353
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-11-13 20:08:08 +0700

    add network2

---
+++ networking/network2/src/main.rs
@@ -0,0 +1,12 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+fn main() {
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8000);
+    println!(
+        "Socket address is {}, port is {}",
+        socket.ip(),
+        socket.port()
+    );
+
+    println!("Is this IPv6 socket? {}", socket.is_ipv6());
+}
