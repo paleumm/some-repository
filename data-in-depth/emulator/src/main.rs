c2ed523998004891fdba87600b97e313c37076cf
paleumm <zdotqdot@gmail.com>
2022-11-26 20:40:22 +0700

    Create main.rs

---
+++ data-in-depth/emulator/src/main.rs
@@ -0,0 +1,17 @@
+mod cpu;
+use cpu::CPU;
+
+fn main() {
+    let mut cpu = CPU {
+        current_operation: 0,
+        registers: [0; 2],
+    };
+
+    // 8 => signifies that the operation involves two registers.
+    // 0 => maps to cpu.registers[0].
+    // 1 => maps to cpu.registers[1].
+    // 4 => indicates addition.
+    cpu.current_operation = 0x8014;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+}
