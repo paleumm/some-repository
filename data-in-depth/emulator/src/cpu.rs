42a6410c53c6db2454d63bc2c28ad516486efb0b
paleumm <zdotqdot@gmail.com>
2022-11-27 01:12:44 +0700

    Update cpu.rs

--- data-in-depth/emulator/src/cpu.rs
+++ data-in-depth/emulator/src/cpu.rs
@@ -1,30 +1,49 @@
 pub struct CPU {
-    pub current_operation: u16, // opcode
-    pub registers: [u8; 2],     // 2 registers
+    pub registers: [u8; 16], // 16 registers
+    pub position_in_memory: usize,
+    pub memory: [u8; 0x1000],
 }
 
 impl CPU {
     pub fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2 // combine 2 opcode bytes into 1 u16
     }
 
     pub fn run(&mut self) {
-        // loop {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
 
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {:04x}", opcode),
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
-        // }
     }
 
     pub fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
