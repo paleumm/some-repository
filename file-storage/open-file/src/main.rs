156af0d5dfe14356598ec908830215e9fb7b9df5
Permpoon B <zdotqdot@gmail.com>
2022-12-18 17:20:09 +0700

    Update main.rs

--- file-storage/open-file/src/main.rs
+++ file-storage/open-file/src/main.rs
@@ -1,9 +1,12 @@
-use std::fs::OpenOptions;
+use std::{fs::OpenOptions, path::PathBuf};
 fn main() {
     let f = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .append(true)
-        .open("./src/main.rs")?;
+        .open("./src/main.rs").unwrap();
+    
+        let hello = PathBuf::from("/tmp/hello.txt");
+        hello.extension();
 }
