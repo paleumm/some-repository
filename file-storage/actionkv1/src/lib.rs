f3d498f9aef9ae5ca782208efc33a028da4c5617
Permpoon B <zdotqdot@gmail.com>
2022-12-19 20:29:22 +0700

    Update lib.rs

--- file-storage/actionkv1/src/lib.rs
+++ file-storage/actionkv1/src/lib.rs
@@ -21,11 +21,31 @@ pub struct ActionKV {
 }
 
 impl ActionKV {
-    pub fn open(path: &Path) -> io::Result<Self> {
-        let f = OpenOptions::new().read(true).write(true).create(true).append(true).open(path)?;
-        let index = HashMap::new();
-        Ok(ActionKV { f: f, index: index })
-    }
+  pub fn open(path: &Path) -> io::Result<Self> {
+    let f = OpenOptions::new().read(true).write(true).create(true).append(true).open(path)?;
+    let index = HashMap::new();
+    Ok(ActionKV { f: f, index: index })
+  }
 
-    pub fn load(&mut self)
+  pub fn load(&mut self) -> io::Result<()> {
+    let mut f = BufReader::new(&mut self.f);
+    loop {
+      let position = f.seek(SeekFrom::Current(0))?;
+      let maybe_kv = ActionKV::process_record(&mut f);
+      let kv = match maybe_kv {
+        Ok(kv) => kv,
+        Err(err) => {
+          match err.kind() {
+            io::ErrorKind::UnexpectedEof => {
+              break;
+            }
+            _=> return Err(err)
+            
+          }
+        }
+      };
+      self.index.insert(kv.key, position);
+    }
+    Ok(())
+  }
 }
