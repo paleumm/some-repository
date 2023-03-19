997309ae64f6ad098e1b5eb3d6542a8f626bebef
paleumm <zdotqdot@gmail.com>
2023-03-20 00:19:17 +0700

    part 2

--- rocket-backend/src/main.rs
+++ rocket-backend/src/main.rs
@@ -1,7 +1,9 @@
+use reqwest::Client;
 use rocket::http::uri::Origin;
 use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::serde::json::{json, Value};
+use rocket::State;
 
 #[macro_use]
 extern crate rocket;
@@ -17,24 +19,41 @@ fn index() -> Redirect {
     ))
 }
 
+const GOOGLE_KEEP_DESKTOP_REPO: &str = "elibroftw/google-keep-desktop-app";
+
+async fn get_latest_release(client: &State<Client>, repo: &str) -> Result<Value, reqwest::Error> {
+    let url = format!("https://api.github.com/repos/{repo}/releases/latest");
+    let response = client.get(&url).send().await?;
+    let github_release = response.json::<Value>().await?;
+    Ok(github_release)
+}
+
 #[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
-fn google_keep_desktop_api(
+async fn google_keep_desktop_api(
     _platform: &str,
     current_version: &str,
     msg: Option<&str>,
+    client: &State<Client>,
 ) -> Result<Value, Status> {
     if let Some(msg) = msg {
         println!("{msg}");
         return Err(Status::NoContent);
     }
-    Ok(json!({
-        "notes":"IT WORKS"
-    }))
+
+    get_latest_release(client, GOOGLE_KEEP_DESKTOP_REPO)
+        .await
+        .or(Err(Status::NoContent))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .manage(
+            reqwest::Client::builder()
+                .user_agent("reqwest")
+                .build()
+                .unwrap(),
+        )
         .mount("/", routes![index])
         .mount(RELEASE_PREFIX, routes![google_keep_desktop_api])
 }
